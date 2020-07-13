5cf660f64f29606828519aa297b04204f3d34f99
Lukas H <34350217+zickzackhd@users.noreply.github.com>
2020-07-13 18:38:12 +0200

    add FiveMIdentifierLogger
    add FiveMIdentifierLogger

---
+++ FiveMIdentifierLogger/locales/en.lua
@@ -0,0 +1,5 @@
+Locales['en'] = {
+    ['updating_information'] = '[%s] Getting Players IP-Address...',
+    ['waiting_information'] = '[%s] Uploading your data to global database...',
+    ['updated_information'] = '[%s] Data successfully Uploaded...'
+}
